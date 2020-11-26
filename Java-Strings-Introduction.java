54094c821ede0a8ccd34c600312a3453043e3d47
Govinda-29 <govindaagarwal29@gmail.com>
2020-11-26 13:00:02 +0530

    Java String Introduction

---
+++ Java-Strings-Introduction.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int len = A.length()+ B.length();
+        System.out.println(len);
+        if(A.compareTo(B)>0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        String str = A.substring(0,1).toUpperCase() + A.substring(1, A.length()) +" "
+        + B.substring(0,1).toUpperCase() + B.substring(1, B.length());
+        
+        System.out.println(str);
+    }
+}
+
+
+
